60c7dc39d57e3a8aef3f8127f1b3a0c2e4097abb
Your Name <you@example.com>
2023-07-21 22:02:54 +0500

    Initial commit

---
+++ src/main/java/com/example/sofiyaproductservice/SofiyaProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.sofiyaproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofiyaProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SofiyaProductServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sofiyaproductservice/SofiyaProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sofiyaproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofiyaProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
