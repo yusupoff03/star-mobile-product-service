4e7ede68dbfdcd9785d6b1b30c27832b1ae2d4d0
Msi Laptop <hh@gg.com>
2023-07-22 11:20:38 +0500

    created BeanConfig, DataNotFoundException, GlobalExceptionHandler,    updated ProductRepository, ProductService

---
+++ src/main/java/com/example/sofiyaproductservice/exception/DataNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.sofiyaproductservice.exception;
+
+public class DataNotFoundException extends RuntimeException {
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}
