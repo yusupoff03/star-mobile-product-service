394d338db1c0fbf398d0db2fadbd52e048b4c24f
ShoxruhMingnazarov <shohruxmngnazarov810@gamil.com>
2023-07-24 16:34:05 +0500

    exceptions updated

--- src/main/java/com/example/sofiyaproductservice/config/GlobalExceptionHandler.java
+++ src/main/java/com/example/sofiyaproductservice/config/GlobalExceptionHandler.java
@@ -1,4 +1,38 @@
 package com.example.sofiyaproductservice.config;
 
+import com.example.sofiyaproductservice.exception.AuthenticationFailedException;
+import com.example.sofiyaproductservice.exception.DataNotFoundException;
+import com.example.sofiyaproductservice.exception.RequestValidationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.naming.AuthenticationException;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = {DataNotFoundException.class})
+    public ResponseEntity<String> dataNotFoundExceptionHandler(
+            DataNotFoundException e){
+        return ResponseEntity.status(401).body(e.getMessage());
+    }
+
+
+    @ExceptionHandler(value = {AuthenticationException.class})
+    public ResponseEntity<String> authenticationFailedException(
+            AuthenticationFailedException e){
+        return ResponseEntity.status(401).body(e.getMessage());
+    }
+
+    @ExceptionHandler(value = {RequestValidationException.class})
+    public ResponseEntity<String> requestValidationException(
+            RequestValidationException e){
+        return ResponseEntity.status(400).body(e.getMessage());
+    }
+    )
+
+
+
+
 }
