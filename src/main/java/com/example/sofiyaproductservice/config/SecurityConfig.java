f03cb4b1dfd9a43a1391456c8327003b51638243
ShoxruhMingnazarov <shohruxmngnazarov810@gamil.com>
2023-07-24 16:53:07 +0500

    Service Repository and Controller updated

---
+++ src/main/java/com/example/sofiyaproductservice/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.example.sofiyaproductservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity()
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+
+}
