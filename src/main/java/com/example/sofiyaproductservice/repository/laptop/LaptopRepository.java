20edf00f984b4f34c78174e57ac1aa55fbf097c7
abduvohidovismoil <abduvohidovi72@gmail.com>
2023-08-17 16:13:42 +0500

    repository changed

--- src/main/java/com/example/sofiyaproductservice/repository/laptop/LaptopRepository.java
+++ src/main/java/com/example/sofiyaproductservice/repository/laptop/LaptopRepository.java
@@ -2,9 +2,10 @@ package com.example.sofiyaproductservice.repository.laptop;
 
 import com.example.sofiyaproductservice.domain.entity.LaptopEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-
+@Repository
 public interface LaptopRepository extends JpaRepository<LaptopEntity, UUID> {
 
 }
--- src/main/java/com/example/sofiyaproductservice/repository/product/ProductRepository.java
+++ src/main/java/com/example/sofiyaproductservice/repository/product/ProductRepository.java
@@ -3,13 +3,14 @@ package com.example.sofiyaproductservice.repository.product;
 import com.example.sofiyaproductservice.domain.entity.ProductEntity;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@RestController
+@Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, UUID> {
     List<ProductEntity> searchProductEntitiesByModelContainingIgnoreCase(String model, Pageable pageable);
 
