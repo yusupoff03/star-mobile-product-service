7b0e00ef847cdaf3bf68d9215ab86d0c3ad1974f
ShoxruhMingnazarov <shohruxmngnazarov810@gamil.com>
2023-08-19 17:23:25 +0500

    PhoneController repository service impl update;

--- src/main/java/com/example/sofiyaproductservice/service/phone/PhoneService.java
+++ src/main/java/com/example/sofiyaproductservice/service/phone/PhoneService.java
@@ -1,9 +1,9 @@
 package com.example.sofiyaproductservice.service.phone;
 
 import com.example.sofiyaproductservice.domain.dto.PhoneDto;
-import com.example.sofiyaproductservice.domain.dto.ProductCreatDto;
+
 import com.example.sofiyaproductservice.domain.entity.PhoneEntity;
-import com.example.sofiyaproductservice.domain.entity.ProductEntity;
+
 
 import java.util.List;
 import java.util.UUID;
